2c87ac6a29edd2331d43ebfd0128320ab2e16e97
Cristian Chaparro <cristianchaparroa@gmail.com>
2023-11-20 15:49:25 -0500

    simple change to test pipelines

--- packages/nextjs/src/app/page.tsx
+++ packages/nextjs/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        ScoreHUB
+        <h2 className="text-2xl font-bold leading-7 sm:truncate sm:text-3xl sm:tracking-tight">Score HUB</h2>
     </main>
   )
 }
